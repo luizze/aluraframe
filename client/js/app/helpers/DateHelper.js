3c02ad3e05ceb55db6a9e1362ebe4faadb68ee99
Luiz Fernando <luiz.html@outlook.com>
2017-02-15 00:13:53 -0200

    2:2, para não precisar lembrar de atualizar a view toda vez que qualquer operação de mudança ocorrer em nosso modelo, nós alteramos ListaNegociacoes e adicionamos armadilhas que são executadas quando determinados métodos são chamados.

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -6,12 +6,13 @@ class DateHelper {
     }
     
     static dataParaTexto(data) {
+
         return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`;
     }
     
     static textoParaData(texto) {
         
-        if(!/\d{4}-\d{2}-\d{2}/.test(texto)) 
+        if (!/\d{4}-\d{2}-\d{2}/.test(texto)) 
             throw new Error('Deve estar no formato aaaa-mm-dd');
              
         return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
--- client/js/app/models/Mensagem.js
+++ client/js/app/models/Mensagem.js
@@ -1,6 +1,6 @@
 class Mensagem {
     
-    constructor(texto='') {
+    constructor(texto = '') {
         
         this._texto = texto;
     }
